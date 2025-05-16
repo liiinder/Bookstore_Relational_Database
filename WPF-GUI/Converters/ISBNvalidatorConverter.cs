9f1296f8ade66c1de7f81491cdaba9d6b81a5d23
Kristoffer Linder <linderkristoffer@hotmail.com>
2025-05-16 13:56:36 +0200

    small changes

--- WPF-GUI/Converters/ISBNvalidatorConverter.cs
+++ WPF-GUI/Converters/ISBNvalidatorConverter.cs
@@ -10,17 +10,23 @@ namespace WPF_GUI.Converters
         {
             SolidColorBrush brush = new SolidColorBrush();
             brush.Color = Color.FromArgb(100, 255, 0, 0);
+
             if (value is string isbn)
             {
                 if (isbn.Length != 13) return brush;
-                if (isbn[..3] == "978" || isbn[..3] == "979")
+                if (isbn[..3] == "978" || isbn[..3] == "979") // [..3] first 3 characters of string
                 {
                     if (long.TryParse(isbn, out long result))
                     {
                         int i = 0;
-                        int checksum = isbn[..^1].Sum(x => (i++ % 2 * 2 + 1) * (x - '0'));
-                        checksum = 10 - (checksum % 10);
-                        if (checksum == (isbn[^1] - '0')) brush.Color = Color.FromArgb(100, 0, 255, 0);
+
+                        int checkdigit = isbn[..^1].Sum(x => (i++ % 2 * 2 + 1) * (x - '0')) % 10;
+                        // (i++ % 2 * 2 + 1) makes an alternating 1 / 3 pattern which the ISBN 13 uses to calculate the check digit.
+                        // https://en.wikipedia.org/wiki/ISBN#ISBN-13_check_digit_calculation
+
+                        if (checkdigit != 0) checkdigit = 10 - checkdigit;
+
+                        if (checkdigit == (isbn[^1] - '0')) brush.Color = Color.FromArgb(100, 0, 255, 0);
                     }
                 }
             }
